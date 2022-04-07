adb649187b967fd409def328bfd07f90afed1451
Anagha <82244272+anaghajoyis@users.noreply.github.com>
2022-04-07 17:22:57 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+nosex=0;
+nosey=0;
 function preload(){
-
+    lipstick= loadImage('https://i.postimg.cc/pWPQbvGC/l1.png')
 }
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
-    ebcam= createCapture(VIDEO);
+    webcam= createCapture(VIDEO);
     webcam.size(300,300);
     webcam.hide();
 }
@@ -12,6 +14,7 @@ function draw(){
     image(webcam,0,0,300,300);
     posenet= ml5.poseNet(webcam,modelLoaded);
     posenet.on('pose', gotpose);
+    image(lipstick,(nosex-15),nosey,25,25)
 }
 function modelLoaded(){
     console.log("Model Loaded :)");
@@ -21,8 +24,10 @@ function gotpose(result){
     console.log(result);
     console.log(result[0].pose.nose.x);
     console.log(result[0].pose.nose.y);
+    nosex=result[0].pose.nose.x;
+    nosey=result[0].pose.nose.y+18;
     }
 }
-function take_snapshot(){
-    save('filterlipstick.png')
+function takesnapshot(){
+    save('lipstickfilter.png');
 }
